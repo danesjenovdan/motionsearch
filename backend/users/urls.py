d91e22afbfebf02c68fcfa46955b667d51ef0684
kevin <kevin.cvetezar@gmail.com>
2021-06-08 13:13:59 +0200

    Add bigger limit for tokens in url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -16,9 +16,9 @@ urlpatterns = [
     url(r'^me/votes$', MyVotes.as_view({'get': 'retrieve'}), name="myVotes"),
     url(r'^(?P<user_pk>\d+)/favorites/$', UserFavoriteMotionsViewSet.as_view({'get': 'retrieve', 'post':'create', 'delete': 'destroy'}), name="user_favorites"),
     url(r'^oauth/', include(('oauth2_provider.urls', 'oauth'), namespace='oauth2_provider')),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', UserViewSet.as_view({'get': 'activate'}), name='activate'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,50})/$', UserViewSet.as_view({'get': 'activate'}), name='activate'),
     url(r'^forgot-password/$', UserViewSet.as_view({'post': 'forgot'}), name='activate'),
-    url(r'^change-password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', UserViewSet.as_view({'put': 'changePassword'}), name='activate'),
+    url(r'^change-password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,50})/$', UserViewSet.as_view({'put': 'changePassword'}), name='activate'),
 ]
 
 urlpatterns += router.urls
